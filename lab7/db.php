08e654afc4c226ca87cfde68c76e2014368b8150
Courteney Sit <courteneycsagan@gmail.com>
2025-11-11 13:47:28 -0500

    Adding JSON column in courses table

--- lab7/db.php
+++ lab7/db.php
@@ -7,12 +7,6 @@ $db   = getenv('DB_NAME') ?: '';
 $user = getenv('DB_USER') ?: '';
 $pass = getenv('DB_PASS') ?: '';
 
-// $host = getenv('DB_HOST') ?: '';
-// $port = getenv('DB_PORT') ?: '';
-// $db   = getenv('DB_NAME') ?: '';
-// $user = getenv('DB_USER') ?: '';
-// $pass = getenv('DB_PASS') ?: '';
-
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host={$host};port={$port};charset={$charset}";
