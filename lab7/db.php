1beb79b97c31b8c2cdfa55525305dacb5a124d45
danasiongsin <144954515+danasiongsin@users.noreply.github.com>
2025-11-11 21:57:33 -0500

    remove secret db stuff

--- lab7/db.php
+++ lab7/db.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = getenv('DB_HOST') ?: 'websystems.cza8yasoo5ed.us-east-2.rds.amazonaws.com';
-$port = getenv('DB_PORT') ?: '3306';
-$db   = getenv('DB_NAME') ?: 'websystems';
-$user = getenv('DB_USER') ?: 'admin';
-$pass = getenv('DB_PASS') ?: 'DGJNTKzzL01Nwk1XFjxH';
+$host = getenv('DB_HOST') ?: '';
+$port = getenv('DB_PORT') ?: '';
+$db   = getenv('DB_NAME') ?: '';
+$user = getenv('DB_USER') ?: '';
+$pass = getenv('DB_PASS') ?: '';
 
 $charset = 'utf8mb4';
 
